7cdfd5e0b8b8e2d6ebbb36052fcbe7fc89f2ca93
colani-sudo <matsenjwacolani@gmail.com>
2024-01-23 00:26:56 +0800

    initial commit

---
+++ src/com/kloudnerdz/Main.java
@@ -0,0 +1,36 @@
+package com.kloudnerdz;
+
+import java.util.Scanner;
+
+/**
+ *  Simple program that will calculate the
+ *  interest that is earned on a dollar amount
+ *  invested at an interest rate of 0.035% for one year.
+ *  Both the interest earned and the investment after
+ *  one year will be printed.
+ */
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        // declare variables
+        double principal;
+        double rate = 0.035;
+        double interest;
+
+        Scanner scanner = new Scanner(System.in);
+
+        // Prompt user for amount
+        System.out.println("Enter the principal: ");
+        principal = scanner.nextDouble();
+
+        // perform calculations
+        interest = principal * rate;
+        principal += interest; // same as principal = principal + rate
+
+        // Output results
+        System.out.printf("The interest earned is $%.2f" +
+                " The value of the investment after one year is $%.2f", interest, principal);
+    }
+}
